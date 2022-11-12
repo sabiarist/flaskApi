b0a4b8526b3dec36c53a4e58a36ad8b8490fba8a
Aristide Sabi <sabiarist@gmail.com>
2022-11-12 16:04:12 +0000

    error solving in main.py func (AttributeError: module 'jwt' has no attribute 'encode')

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import logging
 import datetime
 import functools
 import jwt
+from jose import jwt
 
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
@@ -82,7 +83,7 @@ def auth():
 
     user_data = body
 
-    return jsonify(token=_get_jwt(user_data).decode('utf-8'))
+    return jsonify(token=_get_jwt(user_data))
 
 
 @APP.route('/contents', methods=['GET'])
--- test_main.py
+++ test_main.py
@@ -27,13 +27,14 @@ def test_health(client):
     assert response.status_code == 200
     assert response.json == 'Healthy'
 
-# def test_auth(client):
-#     body = {'email': EMAIL,
-#             'password': PASSWORD}
-#     response = client.post('/auth',
-#                            data=json.dumps(body),
-#                            content_type='application/json')
-#
-#     assert response.status_code == 200
-#     token = response.json['token']
-#     assert token is not None
+
+def test_auth(client):
+    body = {'email': EMAIL,
+            'password': PASSWORD}
+    response = client.post('/auth',
+                           data=json.dumps(body),
+                           content_type='application/json')
+
+    assert response.status_code == 200
+    token = response.json['token']
+    assert token is not None
